01afb2093d6b37c082c2dbbaf7d97cfc2a216266
yannc21 <yannc@jfrog.com>
2021-05-27 16:31:42 +0200

    s301 - lab3 : fixed pipelines names

--- SU-301-Practical-Guide-to-Leveraging-JFrog-Pipelines-for-Jenkins-Users/env_init/init.sh
+++ SU-301-Practical-Guide-to-Leveraging-JFrog-Pipelines-for-Jenkins-Users/env_init/init.sh
@@ -22,27 +22,48 @@ then
      exit 1
 fi
 
-echo "$ADMIN_USER:$ADMIN_PASS"
+if [ -z "$PIPE_TOKEN" ]
+then
+     echo "provide your Pipeline Token"
+     exit 1
+fi
 
-echo "============================"
-echo "$JPD_URL"
-echo "============================"
+if [ -z "$RT_USER" ]
+then
+     echo "provide a username to be created"
+     exit 1
+fi
 
-# create repo
-#echo "[ARTIFACTORY] creating repositories ..."
-# "curl \
+#echo "$ADMIN_USER:$ADMIN_PASS"
+
+# echo "============================"
+# echo "$JPD_URL"
+# echo "============================"
+
+# # create repo
+# echo "[ARTIFACTORY] creating repositories ..."
+# curl \
 #      -XPATCH \
 #      -u $ADMIN_USER:$ADMIN_PASS \
 #      -H "Content-Type: application/yaml" -T artifactory_repo.yaml \
 # $JPD_URL/artifactory/api/system/configuration 
 
-# # create jenkins user
-# echo "[ARTIFACTORY] creating user ..."
+# # create user
+# echo "[ARTIFACTORY] creating jenkins user ..."
 # curl \
 #      -XPUT \
 #      -u $ADMIN_USER:$ADMIN_PASS \
 #      -H "Content-Type: application/json" -T user.json \
 # $JPD_URL/artifactory/api/security/users/jenkins
+
+
+# # create user
+# echo "[ARTIFACTORY] creating user ..."
+# curl \
+#      -XPUT \
+#      -u $ADMIN_USER:$ADMIN_PASS \
+#      -H "Content-Type: application/json" -T user2.json \
+# $JPD_URL/artifactory/api/security/users/$RT_USER
 
 
 # # create permissions for jenkins user
@@ -54,41 +75,65 @@ echo "============================"
 # $JPD_URL/artifactory/api/v2/security/permissions/ci-urs
 
 
+# # create policy
+# echo -e "\n[XRAY] creating policy  ... !"
 # curl \
+#      -XPOST \
 #      -u $ADMIN_USER:$ADMIN_PASS \
 #      -H "Content-Type: application/json" \
-# $JPD_URL/api/v1/binMgr/default/builds
+#      -d @xray_policy.json \
+# $JPD_URL/xray/api/v2/policies
 
-# index build
+
+# # create watch
+# echo "[XRAY] creating watch  ... !"
+# curl \
+#      -XPOST \
+#      -u $ADMIN_USER:$ADMIN_PASS \
+#      -H "Content-Type: application/json" \
+#      -d @xray_watch.json \
+# $JPD_URL/xray/api/v2/watches
+
+# # create JFrog pipelines integrations
+# echo -e "\n[PIPE] creating K8S integration  ... !"
+# curl \
+#      -XPOST \
+#      -H "Authorization: Bearer $PIPE_TOKEN" \
+#      -H "Content-Type: application/json" \
+#      -d @integration_k8s.json \
+# $JPD_URL/pipelines/api/v1/projectIntegrations
+
+# echo "[PIPE] creating SSH integration  ... !"
+# curl \
+#      -XPOST \
+#      -H "Authorization: Bearer $PIPE_TOKEN" \
+#      -H "Content-Type: application/json" \
+#      -d @integration_sshkey.json \
+# $JPD_URL/pipelines/api/v1/projectIntegrations
+
+# curl \
+#      -XPOST \
+#      -H "Authorization: Bearer $PIPE_TOKEN" \
+#      -H "Content-Type: application/json" \
+#      -d @extension.json \
+# $JPD_URL/pipelines/api/v1/extensionsources
+
+# curl \
+#      -H "Authorization: Bearer $PIPE_TOKEN" \
+#      -H "Content-Type: application/json" \
+# $JPD_URL/pipelines/api/v1/projectIntegrations
+
+## index build
 # echo "{\"names\":[\"`echo $BUILD_NAMES | sed 's/,/","/g'`\"]}"
 # echo -e "\n[XRAY] indexing builds ... "
 # curl \
 #      -XPOST \
 #      -u $ADMIN_USER:$ADMIN_PASS \
 #      -H "Content-Type: application/json" \
-#      -d "@test.json" \
+#      -d "{\"names\":[\"`echo $BUILD_NAMES | sed 's/,/","/g'`\"]}" \
 # $JPD_URL/xray/api/v1/binMgr/builds
 
-#     -d "{\"names\":[\"`echo $BUILD_NAMES | sed 's/,/","/g'`\"]}" \
-
-
-# create policy
-echo -e "\n[XRAY] creating policy  ... !"
-curl \
-     -XPOST \
-     -u $ADMIN_USER:$ADMIN_PASS \
-     -H "Content-Type: application/json" \
-     -d @xray_policy.json \
-$JPD_URL/xray/api/v2/policies
-
-
-# create watch
-echo -e "\n[XRAY] creating watch  ... !"
-curl \
-     -XPOST \
-     -u $ADMIN_USER:$ADMIN_PASS \
-     -H "Content-Type: application/json" \
-     -d @xray_watch.json \
-$JPD_URL/xray/api/v2/watches
-
-
+# curl \
+#      -u $ADMIN_USER:$ADMIN_PASS \
+#      -H "Content-Type: application/json" \
+# $JPD_URL/api/v1/binMgr/default/builds
